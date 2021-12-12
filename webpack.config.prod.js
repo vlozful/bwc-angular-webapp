56b813370c720969e5e399f2857bcb1164d2eac0
Victor Lozovenko <lloozz@gmail.com>
2021-12-12 04:27:48 +0200

    fixed prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,13 +2,11 @@
 
 // Modules
 const path = require("path");
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SpritesmithPlugin = require('webpack-spritesmith');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const apiMocker = require('connect-api-mocker');
-const ClosureCompiler = require('closure-webpack-plugin');
+const ClosurePlugin = require('closure-webpack-plugin');
 
 var outputPath = "./build/prod/"
 
@@ -22,115 +20,126 @@ module.exports = {
         path: path.resolve(__dirname, outputPath),
         publicPath: ""
     },
+    optimization: {
+      minimizer: [
+        new ClosurePlugin({mode: 'AGGRESSIVE_BUNDLE'}, {
+          // compiler flags here
+          //
+          // for debugging help, try these:
+          //
+          // formatting: 'PRETTY_PRINT'
+          // debug: true,
+          // renaming: false
+        })
+      ]
+    },
     devServer: {
-        before: function(app, server) {
-            app.use(apiMocker('/api', {
-                target: 'src/mocks/api',
-                verbose: true
-              }));
-        },
-        contentBase: path.join(__dirname, outputPath),
-        compress: true,
-        overlay: false,
-        stats: {
-          colors: true
-        }
+      onBeforeSetupMiddleware: function(server) {
+        server.app.use(apiMocker('/api', {
+            target: 'src/mocks/api',
+            verbose: true
+          }));
+      },
+      compress: true,
+      hot: true
     },
     devtool: "source-map",
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['auto-ngtemplate-loader']
-            },
-            {
-                test: /\.tmpl.html$/,
-                exclude: /node_modules/,
-                use: [
-                  {
-                    loader: 'ngtemplate-loader',
-                    options: {
-                      relativeTo: 'src/templates/'
-                    }
-                  },
-                  {
-                    loader: 'html-loader'
+      rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: ['auto-ngtemplate-loader']
+          },
+          {
+              test: /\.tmpl.html$/,
+              exclude: /node_modules/,
+              use: [
+                {
+                  loader: 'ngtemplate-loader',
+                  options: {
+                    relativeTo: 'src/templates/'
                   }
-                ]
-              },
-              {
-                test: /(browserconfig.xml|manifest.json)/,
-                type: 'javascript/auto',
-                use: [
-                  {
-                    loader: "file-loader",
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'i/favicons'
-                    },
-                  },
-                  {
-                    loader: path.resolve('./src/webpack/favicons-manifest-loader/index.js')
-                  },
-                ]
-              },
-              {
-                test: /\.(ico|png|jpg|gif|svg)$/,
-                exclude: /sprites-gen/,
-                use: [{
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext]?[hash]',
-                            context: 'src'
-                        }
-                    }]
-              },
-              {
-                test: /(sprites-gen).*\.(png)$/,
-                use: [{
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]?[hash]',
-                            context: 'src',
-                            outputPath: 'i'
-                        }
-                    }]
-              },
-              {
-                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts'
-                    }
-                }]
+                },
+                {
+                  loader: 'html-loader'
+                }
+              ]
             },
             {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader" // creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader", // translates CSS into CommonJS
-                        options: {
-                            url: true
-                        }
-                    },
-                    {
-                    loader: "sass-loader", // compiles Sass to CSS
-                    options:{
+              test: /(browserconfig.xml|manifest.json)/,
+              type: 'javascript/auto',
+              use: [
+                {
+                  loader: "file-loader",
+                  options: {
+                    esModule: false,
+                    name: '[name].[ext]',
+                    outputPath: 'i/favicons'
+                  },
+                },
+                {
+                  loader: path.resolve('./src/webpack/favicons-manifest-loader/index.js'),
+                  options: {
+                    outputPath: 'i/favicons'
+                  }
+                },
+              ]
+            },
+            {
+              test: /\.(ico|png|jpg|gif|svg)$/,
+              exclude: [/sprites-gen/, /favicons/],
+              type: 'asset/resource',
+              generator: {
+                filename: 'i/[name][ext]?[hash]]'
+              }
+            },
+            {
+              test: /(favicons).*.(ico|png|jpg|gif|svg)$/,
+              type: 'asset/resource',
+              generator: {
+                filename: 'i/favicons/[name][ext]?[hash]]'
+              }
+            },
+            {
+              test: /(sprites-gen).*\.(png)$/,
+              type: 'asset/resource',
+              generator: {
+                filename: 'i/[name][ext]?[hash]]'
+              }
+            },
+            {
+              test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+              type: 'asset/resource',
+              generator: {
+                filename: 'fonts/[name][ext]?[hash]]'
+              }
+          },
+          {
+              test: /\.scss$/,
+              use: [
+                  {
+                      loader: "style-loader" // creates style nodes from JS strings
+                  },
+                  {
+                      loader: "css-loader", // translates CSS into CommonJS
+                      options: {
+                          url: true
+                      }
+                  },
+                  {
+                  loader: "sass-loader", // compiles Sass to CSS
+                  options: {
+                      sassOptions: {
                         includePaths: [
                           path.resolve(__dirname, './node_modules/compass-mixins/lib'),
                           path.resolve(__dirname, './src/sprites-gen')
-                        ]
-                        }
-                    }
-                ]
-            }
-        ],
+                        ]}
+                      }
+                  }
+              ]
+          }
+      ],
     },
     optimization: {
       /*
